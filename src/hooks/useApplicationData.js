d14c9c14fd2c10e49571ba54924ff93366e015e4
Martin Halas <echoes.time@gmail.com>
2021-04-18 13:41:36 -0400

    Added function to update spots remaining state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import { cloneDeep } from "lodash";
 
 export default function useApplicationData() {
-  
+  //state.days.appointmentId.spots
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -13,6 +14,8 @@ export default function useApplicationData() {
 
   const setDay = day => setState({...state, day});
   
+  const dayIndex = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].indexOf(state.day);
+
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
@@ -23,9 +26,14 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-  
+    // state.days[dayIndex].spots
+
+    // returns state.days copy where spots is reduced by 1 for target day
+    const days = cloneDeep(state.days);
+    days[dayIndex].spots--
+
     return axios.put(`/api/appointments/${id}`, { interview })
-    .then(() => setState({...state, appointments }))
+    .then(() => setState({...state, appointments, days }))
   }
   
   const cancelInterview = id => {
@@ -34,8 +42,12 @@ export default function useApplicationData() {
       [id]: { ...state.appointments[id], interview: null }
     }
   
+    // returns state.days copy where spots is increased by 1 for target day
+    const days = cloneDeep(state.days);
+    days[dayIndex].spots++
+
     return axios.delete(`/api/appointments/${id}`)
-    .then(() => setState({ ...state, appointments}));
+    .then(() => setState({ ...state, appointments, days }));
   }
 
   const daysPromise = Promise.resolve(axios.get("/api/days"));
@@ -47,7 +59,6 @@ export default function useApplicationData() {
         setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
       })
   }, [])
-
 
   return { state: state, setDay, bookInterview, cancelInterview}
 }
